4ef5b19151f5fa28bbd394c168c1de81a16e1227
apeden <apeden23@gmail.com>
2018-08-21 22:32:50 +0100

    now I've added the test harness

---
+++ the_100_problem_test_harnessv2.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  6 00:10:32 2018
+
+@author: user
+"""
+from the_100_problemv2 import *
+import random
+
+# def num_dict text
+#tests =(1,2,9,20)
+#for i in tests:
+#    print(num_dict(i))
+    
+#list_join test
+#tests = {
+#1:['6',' ','7',' ','8',' ','9'],
+#2:['+','-','1','2','3'   ,' ','4'],
+#3:['8','9'],
+#4:['1','2','3',' + ','4','5','6']}
+#for i in tests:
+#    print(list_join(tests[i]))
+
+#computation test
+#test = {
+#1:'1 + 234 - 678 + 9', 2:'1 + 2 + 3 + 4 - 5 + 6 - 7 + 8 - 9', 3:'12345 - 6789'}
+#for i in test:
+#    print(computation(test[i]))
+    #print(test[1])
+
+
+#add ops test
+#dict_list_a = {0:['1','2','3']}
+#dict_list_b = add_ops(dict_list_a)
+##print(dict_list_b)
+##print(len(dict_list_b))
+#
+#dict_list_c = add_ops(dict_list_b)
+##print(dict_list_c)
+##print(len(dict_list_c))
+#
+#dict_list_d = add_ops(dict_list_c)
+#print(dict_list_d)
+#print(len(dict_list_d))
+
+
+
+#dict_list_1 = {0:['1','2','3','4','5','6','7','8','9']}
+#dict_list_2 = add_ops(dict_list_1)
+##print(dict_list_2)
+##print(len(dict_list_2))
+#    
+#dict_list_3 = add_ops(dict_list_2)
+##print(dict_list_3)
+##print(len(dict_list_3))
+#    
+#dict_list_4 = add_ops(dict_list_3)
+##print(dict_list_4)
+##print(len(dict_list_4))
+#    
+#dict_list_5 = add_ops(dict_list_4)
+##print(dict_list_5)
+##print(len(dict_list_5))
+#
+#dict_list_6 = add_ops(dict_list_5)
+##print(dict_list_6)
+##print(len(dict_list_6))
+#
+#dict_list_7 = add_ops(dict_list_6)
+#print(dict_list_7)
+#print(len(dict_list_7))
+
+
+#search dict test
+#target = 1234584
+#dict_list1 = {0:['1','2','3','4','6','8','4',],1:['1','2','3','4','5','6','7',' + ','8',' + ','9']}
+#print(search_dict(dict_list1, target))
+
+#system search test
+system_search(10,10)
